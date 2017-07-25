467c5288d2bcb66400aa7acc1344081cbc5ad1de
Ete Remi <remi.ete@desy.de>
2017-07-25 19:45:23 +0200

    Added getParameter() function. Not tested yet, end day commit ...

--- calibration/CalibrationStep.py
+++ calibration/CalibrationStep.py
@@ -32,5 +32,27 @@ class CalibrationStep(object) :
             for elt in elts :
                 tree.getroot().remove(elt)
 
+    def getParameter(self, config, name, step=None) :
 
+        userInput = config.xpath("//input")
+        stepElt = None
+
+        if step is not None :
+            stepElts = config.xpath("//step[@name='{0}']".format(step))
+
+            if len(stepElts) :
+                stepElt = stepElts[-1].find("output")
+
+        if not len(userInput) and stepElt is None :
+            raise RuntimeError("Couldn't get parameter '{0}' from input nor from step".format(name))
+
+        if len(userInput) :
+            userInput = userInput[0]
+
+        # first look into the step element
+        if stepElt :
+            return stepElt.find(name).text
+
+        # then look into user input element
+        return userInput.find(name).text
 #
