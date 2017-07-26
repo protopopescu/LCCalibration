b1820d79ee157dc52d809d5c63a17b2f77d36399
Ete Remi <remi.ete@desy.de>
2017-07-26 18:07:24 +0200

    getFileContent() added + changes in all functions. Added get functions for incoming steps ...

--- calibration/FileTools.py
+++ calibration/FileTools.py
@@ -1,40 +1,28 @@
 import linecache
 
-def getHcalBarrelMip(calibFile) :
-    line = linecache.getline(calibFile, 7)
+def getFileContent(fname, lid, tokenid) :
+    line = linecache.getline(fname, lid)
     lineTokens = line.split()
     linecache.clearcache()
-    return float(lineTokens[5])
+    return lineTokens[tokenid]
+
+def getHcalBarrelMip(calibFile) :
+    return float(getFileContent(calibFile, 7, 5))
 
 def getHcalEndcapMip(calibFile) :
-    line = linecache.getline(calibFile, 8)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[5])
+    return float(getFileContent(calibFile, 8, 5))
 
 def getHcalRingMip(calibFile) :
-    line = linecache.getline(calibFile, 9)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[5])
+    return float(getFileContent(calibFile, 9, 5))
 
 def getEcalMip(calibFile) :
-    line = linecache.getline(calibFile, 10)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[4])
+    return float(getFileContent(calibFile, 10, 4))
 
 def getTruePhotonEnergy(calibFile) :
-    line = linecache.getline(calibFile, 4)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[4])
+    return float(getFileContent(calibFile, 4, 4))
 
 def getEcalDigiMean(calibFile) :
-    line = linecache.getline(calibFile, 9)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[4])
+    return float(getFileContent(calibFile, 9, 4))
 
 def getEcalRescalingFactor(calibFile) :
     ten = getTruePhotonEnergy(calibFile)
@@ -42,15 +30,26 @@ def getEcalRescalingFactor(calibFile) :
     return ten/digi
 
 def getHcalDigiMean(calibFile) :
-    line = linecache.getline(calibFile, 9)
-    lineTokens = line.split()
-    linecache.clearcache()
-    return float(lineTokens[5])
+    return float(getFileContent(calibFile, 9, 5))
 
 def getHcalRescalingFactor(calibFile, trueKaon0LEnergy) :
     hcalDigiMean = getHcalDigiMean(calibFile)
     return trueKaon0LEnergy / hcalDigiMean
 
+def getMeanDirCorrHcalEndcap(calibFile) :
+    return float(getFileContent(calibFile, 4, 5))
+
+def getMeanDirCorrHcalRing(calibFile) :
+    return float(getFileContent(calibFile, 9, 5))
+
+def getEcalToGeVMip(calibFile) :
+    return float(getFileContent(calibFile, 8, 2))
+
+def getHcalToGeVMip(calibFile) :
+    return float(getFileContent(calibFile, 16, 2))
+
+def getMuonToGeVMip(calibFile) :
+    return float(getFileContent(calibFile, 24, 2))
 
 
 
