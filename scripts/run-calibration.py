ed13d727dac761cfd15a91675a24fb8ce3259cf9
Ete Remi <remi.ete@desy.de>
2017-08-14 16:51:18 +0200

    typo : PANDORAANALYSIS_DIR -> PANDORA_ANALYSIS_DIR

--- scripts/run-calibration.py
+++ scripts/run-calibration.py
@@ -43,7 +43,7 @@ hcalEndcapRegionRange = "0.79:0.98"
 
 # try to get from env vars
 try:
-    pathToPandoraAnalysis = os.environ["PANDORAANALYSIS_DIR"]
+    pathToPandoraAnalysis = os.environ["PANDORA_ANALYSIS_DIR"]
 except KeyError:
     pass
 
@@ -146,7 +146,7 @@ parser.add_argument("--lcioMuonFile", action="store", default=lcioMuonFile,
                         help="The lcio input file containing muons to process", required = False)
 
 parser.add_argument("--pandoraAnalysis", action="store", default=pathToPandoraAnalysis,
-                        help="The path to the PandoraAnalysis package", required = True)
+                        help="The path to the PandoraAnalysis package (default : '{0}')".format(pathToPandoraAnalysis), required = True)
 
 parser.add_argument("--maxRecordNumber", action="store", default=maxRecordNumber,
                         help="The maximum number of events to process", required = False)
