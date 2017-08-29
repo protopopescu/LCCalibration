36eb9df8bd0cab57d7fb5f9a8d57bbb644197b2b
Ete Remi <remi.ete@desy.de>
2017-08-29 13:33:40 +0200

    Changed import order to get the correct library on nafhh

--- calibration/XmlTools.py
+++ calibration/XmlTools.py
@@ -5,22 +5,22 @@ try:
 except ImportError:
     try:
         # Python 2.5
-        import xml.etree.cElementTree as etree
-        print("running with cElementTree on Python 2.5+")
+        import xml.etree.ElementTree as etree
+        print("running with ElementTree on Python 2.5+")
     except ImportError:
         try:
             # Python 2.5
-            import xml.etree.ElementTree as etree
-            print("running with ElementTree on Python 2.5+")
+            import xml.etree.cElementTree as etree
+            print("running with cElementTree on Python 2.5+")
         except ImportError:
             try:
-                # normal cElementTree install
-                import cElementTree as etree
-                print("running with cElementTree")
+                # normal ElementTree install
+                import elementtree.ElementTree as etree
+                print("running with ElementTree")
             except ImportError:
                 try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-                    print("running with ElementTree")
+                    # normal cElementTree install
+                    import cElementTree as etree
+                    print("running with cElementTree")
                 except ImportError:
                     print("Failed to import ElementTree from any known place")
