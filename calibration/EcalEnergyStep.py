87b5232c9723435076d4f2bea8805b432fff642b
Ete Remi <remi.ete@desy.de>
2017-09-20 11:20:02 +0200

    Split ecal calib into two classes : one for ILDCaloDigi, one for split processor chain

--- calibration/EcalEnergyStep.py
+++ calibration/EcalEnergyStep.py
@@ -7,7 +7,8 @@ from calibration.FileTools import *
 import os, sys
 from calibration.XmlTools import etree
 
-
+""" EcalEnergyStep class. Base class for calibrating the ecal
+"""
 class EcalEnergyStep(CalibrationStep) :
     def __init__(self) :
         CalibrationStep.__init__(self, "EcalEnergy")
@@ -16,24 +17,20 @@ class EcalEnergyStep(CalibrationStep) :
         self._maxNIterations = 5
         self._energyScaleAccuracy = 0.01
 
-        self._inputEcalBarrelFactor1 = None
-        self._inputEcalBarrelFactor2 = None
-        self._inputEcalEndcapFactor1 = None
-        self._inputEcalEndcapFactor2 = None
-        self._inputEcalRingFactor1 = None
-        self._inputEcalRingFactor2 = None
         self._inputEcalRingGeometryFactor = None
         self._inputMinCosThetaBarrel = None
         self._inputMaxCosThetaBarrel = None
         self._inputMinCosThetaEndcap = None
         self._inputMaxCosThetaEndcap = None
 
-        self._outputEcalBarrelFactor1 = None
-        self._outputEcalBarrelFactor2 = None
-        self._outputEcalEndcapFactor1 = None
-        self._outputEcalEndcapFactor2 = None
-        self._outputEcalRingFactor1 = None
-        self._outputEcalRingFactor2 = None
+        self._outputEcalBarrelFactors = None
+        self._outputEcalEndcapFactors = None
+        self._outputEcalRingFactors = None
+        
+        self._runProcessors = []
+        self._pfoAnalysisProcessor = "MyPfoAnalysis"
+        self._loadStepOutputs = []
+        self._runRingCalibration = True
         
         # command line requirement
         self._requireSteeringFile()
@@ -42,10 +39,48 @@ class EcalEnergyStep(CalibrationStep) :
         self._requirePhotonFile()
         self._requireECalAccuracy()
 
+    """ Set the processor list to run only
+    """
+    def setRunProcessors(self, processors):
+        self._runProcessors = list(processors)
+        
+    """ Set the pfo analysis processor name in the reco chain
+    """
+    def setPfoAnalysisProcessor(self, pfoAnalysis):
+        self._pfoAnalysisProcessor = str(pfoAnalysis)
+    
+    """ The (optional) steps output to load before processing this step
+    """
+    def setLoadStepOutputs(self, steps):
+        self._loadStepOutputs = list(steps)
+    
+    """ Whether to run the ecal ring calibration
+    """
+    def setRunEcalRingCalibration(self, runRingCalib):
+        self._runRingCalibration = runRingCalib
+    
+    """ Should return the current list of ecal barrel energy factors from marlin xml file 
+    """
+    def ecalBarrelEnergyFactors(self):
+        return []
+        
+    """ Should return the current list of ecal barrel energy factors from marlin xml file 
+    """    
+    def ecalEndcapEnergyFactors(self):
+        return []
 
+    """ Should set the current list of ecal barrel/endcap energy factors into marlin xml file 
+    """    
+    def setEnergyFactors(self, barrelFactors, endcapFactors):
+        pass
+    
+    """ Get the step description
+    """
     def description(self):
         return "Calculate the constants related to the energy deposit in a ecal cell (unit GeV). Outputs the ecalFactors values"
 
+    """ Read the command line arguments
+    """
     def readCmdLine(self, parsed) :
         # setup marlin
         self._marlin = Marlin(parsed.steeringFile)
@@ -62,35 +97,21 @@ class EcalEnergyStep(CalibrationStep) :
         self._inputMinCosThetaBarrel, self._inputMaxCosThetaBarrel = self._getGeometry().getEcalBarrelCosThetaRange()
         self._inputMinCosThetaEndcap, self._inputMaxCosThetaEndcap = self._getGeometry().getEcalEndcapCosThetaRange()
 
+    """ Initialize the calibration step
+    """
     def init(self, config) :
-        # list of processors to run
-        processors = []
-        processors.extend(["MyAIDAProcessor"]) # not sure this is needed ...
-        processors.extend(["InitDD4hep"])
-        processors.extend(["MyEcalBarrelDigi", "MyEcalBarrelReco", "MyEcalBarrelGapFiller"])
-        processors.extend(["MyEcalEndcapDigi", "MyEcalEndcapReco", "MyEcalEndcapGapFiller"])
-        processors.extend(["MyEcalRingDigi", "MyEcalRingReco"])
-        processors.extend(["MyHcalBarrelDigi", "MyHcalBarrelReco"])
-        processors.extend(["MyHcalEndcapDigi", "MyHcalEndcapReco"])
-        processors.extend(["MyHcalRingDigi", "MyHcalRingReco"])
-        processors.extend(["MySimpleBCalDigi", "MySimpleLCalDigi", "MySimpleLHCalDigi", "MySimpleMuonDigi"])
-        processors.extend(["MyPfoAnalysis"])
-        
         self._cleanupElement(config)
         self._marlin.loadInputParameters(config)
-        self._marlin.loadStepOutputParameters(config, "MipScale")
-        self._marlin.turnOffProcessorsExcept(processors)
-    
-        self._inputEcalBarrelFactor1 = float(self._marlin.getProcessorParameter("MyEcalBarrelReco", "calibration_factorsMipGev").split()[0])
-        self._inputEcalBarrelFactor2 = float(self._marlin.getProcessorParameter("MyEcalBarrelReco", "calibration_factorsMipGev").split()[1])
-        self._inputEcalEndcapFactor1 = float(self._marlin.getProcessorParameter("MyEcalEndcapReco", "calibration_factorsMipGev").split()[0])
-        self._inputEcalEndcapFactor2 = float(self._marlin.getProcessorParameter("MyEcalEndcapReco", "calibration_factorsMipGev").split()[1])
-        self._inputEcalRingFactor1 = float(self._marlin.getProcessorParameter("MyEcalRingReco", "calibration_factorsMipGev").split()[0])
-        self._inputEcalRingFactor2 = float(self._marlin.getProcessorParameter("MyEcalRingReco", "calibration_factorsMipGev").split()[1])
+        
+        for step in self._loadStepOutputs:
+            self._marlin.loadStepOutputParameters(config, step)
+        
+        if len(self._runProcessors):
+            self._marlin.turnOffProcessorsExcept(self._runProcessors)
 
-
+    """ Run the calibration step
+    """
     def run(self, config) :
-
         # loop variables
         barrelCurrentPrecision = 0.
         endcapCurrentPrecision = 0.
@@ -103,10 +124,8 @@ class EcalEnergyStep(CalibrationStep) :
         barrelAccuracyReached = False
         endcapAccuracyReached = False
 
-        ecalBarrelFactor1 = self._inputEcalBarrelFactor1
-        ecalBarrelFactor2 = self._inputEcalBarrelFactor2
-        ecalEndcapFactor1 = self._inputEcalEndcapFactor1
-        ecalEndcapFactor2 = self._inputEcalEndcapFactor2
+        ecalBarrelFactors = self.ecalBarrelEnergyFactors()
+        ecalEndcapFactors = self.ecalEndcapEnergyFactors()
 
         pfoAnalysisFile = ""
         
@@ -116,19 +135,18 @@ class EcalEnergyStep(CalibrationStep) :
 
             # readjust iteration parameters
             if not barrelAccuracyReached:
-                ecalBarrelFactor1 = ecalBarrelFactor1*barrelRescaleFactor
-                ecalBarrelFactor2 = ecalBarrelFactor2*barrelRescaleFactor
+                for index in range(len(ecalBarrelFactors)):
+                    ecalBarrelFactors[index] = ecalBarrelFactors[index]*barrelRescaleFactor
             
             if not endcapAccuracyReached:
-                ecalEndcapFactor1 = ecalEndcapFactor1*endcapRescaleFactor
-                ecalEndcapFactor2 = ecalEndcapFactor2*endcapRescaleFactor
+                for index in range(len(ecalEndcapFactors)):
+                    ecalEndcapFactors[index] = ecalEndcapFactors[index]*endcapRescaleFactor
 
             pfoAnalysisFile = "./PfoAnalysis_{0}_iter{1}.root".format(self._name, iteration)
-
-            # run marlin 
-            self._marlin.setProcessorParameter("MyEcalBarrelReco", "calibration_factorsMipGev", "{0} {1}".format(ecalBarrelFactor1, ecalBarrelFactor2))
-            self._marlin.setProcessorParameter("MyEcalEndcapReco", "calibration_factorsMipGev", "{0} {1}".format(ecalEndcapFactor1, ecalEndcapFactor2))
-            self._marlin.setProcessorParameter("MyPfoAnalysis"   , "RootFile", pfoAnalysisFile)
+            
+            # run marlin
+            self.setEnergyFactors(ecalBarrelFactors, ecalEndcapFactors)
+            self._marlin.setProcessorParameter(self._pfoAnalysisProcessor, "RootFile", pfoAnalysisFile)
             self._marlin.run()
 
             # run calibration for barrel
@@ -147,7 +165,7 @@ class EcalEnergyStep(CalibrationStep) :
             if not endcapAccuracyReached:
                 ecalCalibrator.setRootFile(pfoAnalysisFile)
                 ecalCalibrator.setDetectorRegion("EndCap")
-                ecalCalibrator.setCosThetaRange(self._inputMinCosThetaBarrel, self._inputMaxCosThetaBarrel)
+                ecalCalibrator.setCosThetaRange(self._inputMinCosThetaEndcap, self._inputMaxCosThetaEndcap)
                 ecalCalibrator.run()
                                 
                 newEndcapPhotonEnergy = ecalCalibrator.getEcalDigiMean()
@@ -157,7 +175,7 @@ class EcalEnergyStep(CalibrationStep) :
             
             self._logger.info("=============================================")
             self._logger.info("======= Barrel output for iteration {0} =======".format(iteration))
-            self._logger.info(" => calibrationFactors : {0}, {1}".format(ecalBarrelFactor1, ecalBarrelFactor2))
+            self._logger.info(" => calibrationFactors : {0}".format(", ".join(map(str, ecalBarrelFactors))))
             self._logger.info(" => calibrationRescaleFactor : " + str(barrelRescaleFactor))
             self._logger.info(" => calibrationRescaleFactorCumul : " + str(barrelRescaleFactorCumul))
             self._logger.info(" => currentPrecision : " + str(barrelCurrentPrecision))
@@ -166,14 +184,12 @@ class EcalEnergyStep(CalibrationStep) :
             self._logger.info("")
             self._logger.info("=============================================")
             self._logger.info("======= Endcap output for iteration {0} =======".format(iteration))
-            self._logger.info(" => calibrationFactors : {0}, {1}".format(ecalEndcapFactor1, ecalEndcapFactor2))
+            self._logger.info(" => calibrationFactors : {0}".format(", ".join(map(str, ecalEndcapFactors))))
             self._logger.info(" => calibrationRescaleFactor : " + str(endcapRescaleFactor))
             self._logger.info(" => calibrationRescaleFactorCumul : " + str(endcapRescaleFactorCumul))
             self._logger.info(" => currentPrecision : " + str(endcapCurrentPrecision))
             self._logger.info(" => newPhotonEnergy : " + str(newEndcapPhotonEnergy))
             self._logger.info("=============================================")
-
-
 
             # write down iteration results
             self._writeIterationOutput(config, iteration,
@@ -188,14 +204,12 @@ class EcalEnergyStep(CalibrationStep) :
             # are we accurate enough ??
             if barrelCurrentPrecision < self._energyScaleAccuracy and not barrelAccuracyReached:
                 barrelAccuracyReached = True
-                self._outputEcalBarrelFactor1 = ecalBarrelFactor1
-                self._outputEcalBarrelFactor2 = ecalBarrelFactor2
+                self._outputEcalBarrelFactors = ecalBarrelFactors
 
             # are we accurate enough ??
             if endcapCurrentPrecision < self._energyScaleAccuracy and not endcapAccuracyReached:
                 endcapAccuracyReached = True
-                self._outputEcalEndcapFactor1 = ecalEndcapFactor1
-                self._outputEcalEndcapFactor2 = ecalEndcapFactor2
+                self._outputEcalEndcapFactors = ecalEndcapFactors
 
             if barrelAccuracyReached and endcapAccuracyReached :
                 break
@@ -205,34 +219,126 @@ class EcalEnergyStep(CalibrationStep) :
 
         self._logger.info("{0}: ecal energy accuracy reached !".format(self._name))
 
-        ecalRingCalibrator = EcalRingCalibrator()
-        ecalRingCalibrator.setKaon0LEnergy(20)
-        ecalRingCalibrator.setRootFile(pfoAnalysisFile)
+        if self._runRingCalibration:
+            # TODO evaluate consistency of making separate ecal ring calibration
+            self._outputEcalRingFactors = list(self._outputEcalEndcapFactors)
 
-        directionCorrectionEndcap = ecalRingCalibrator.getEndcapMeanDirectionCorrection()
-        directionCorrectionRing = ecalRingCalibrator.getRingMeanDirectionCorrection()
-        directionCorrectionRatio = directionCorrectionEndcap / directionCorrectionRing
+            self._logger.info("===============================================")
+            self._logger.info("==== Ecal ring output after all iterations ====")
+            self._logger.info(" => ring calib factors : {0}".format(", ".join(map(str, self._outputEcalRingFactors))))
+            self._logger.info("===============================================")
 
-        # compute hcal ring factor
-        # FIXME Do we have to compute this or not ?
-        # Does it make sense or not ???
-        # mipRatio = self._ecalEndcapMip / self._ecalRingMip
-        self._outputEcalRingFactor1 = directionCorrectionRatio * self._outputEcalEndcapFactor1 * self._inputEcalRingGeometryFactor # *mipRatio
-        self._outputEcalRingFactor2 = directionCorrectionRatio * self._outputEcalEndcapFactor2 * self._inputEcalRingGeometryFactor # *mipRatio
+    """ Write output (must be reimplemented)
+    """
+    def writeOutput(self, config):
+        raise RuntimeError("EcalEnergyStep.writeOutput: method not implemented !")
 
-        self._logger.info("===============================================")
-        self._logger.info("==== Ecal ring output after all iterations ====")
-        self._logger.info(" => ring calib factors : {0}, {1}".format(self._outputEcalRingFactor1, self._outputEcalRingFactor2))
-        self._logger.info("===============================================")
+################################################################################
+""" SplitRecoEcalEnergyStep class.
+    Implements the ecal calibration based on the split digitization processors,
+    one processor per ecal region : barrel, endcap and ring.
+    Ring calibration can be switched off by setting the processor name with None,
+    i.e setEcalRecoNames("EcalBarrelDigi", "EcalEndcapDigi", None)
+"""
+class SplitRecoEcalEnergyStep(EcalEnergyStep):
+    def __init__(self):
+        EcalEnergyStep.__init__(self)
+        self._ecalRecoNames = ["MyEcalBarrelReco", "MyEcalEndcapReco", "MyEcalRingReco"]
 
+    """ Set the ecal hit reconstruction processor names. Set None to disable a processor 
+    """
+    def setEcalRecoNames(self, barrelReco, endcapReco, ringReco):
+        self._ecalRecoNames[0] = barrelReco if isinstance(barrelReco, str) else None
+        self._ecalRecoNames[1] = endcapReco if isinstance(endcapReco, str) else None
+        self._ecalRecoNames[2] = ringReco if isinstance(ringReco, str) else None
+    
+    """ Write step output
+    """
     def writeOutput(self, config) :
         output = self._getXMLStepOutput(config, create=True)
 
-        self._writeProcessorParameter(output, "MyEcalBarrelReco", "calibration_factorsMipGev", "{0} {1}".format(self._outputEcalBarrelFactor1, self._outputEcalBarrelFactor2))
-        self._writeProcessorParameter(output, "MyEcalEndcapReco", "calibration_factorsMipGev", "{0} {1}".format(self._outputEcalEndcapFactor1, self._outputEcalEndcapFactor2))
-        self._writeProcessorParameter(output, "MyEcalRingReco",   "calibration_factorsMipGev", "{0} {1}".format(self._outputEcalRingFactor1, self._outputEcalRingFactor2))
+        if self._ecalRecoNames[0] and len(self._outputEcalBarrelFactors):
+            self._writeProcessorParameter(output, self._ecalRecoNames[0], "calibration_factorsMipGev", " ".join(map(str, self._outputEcalBarrelFactors)))
+        
+        if self._ecalRecoNames[1] and len(self._outputEcalEndcapFactors):
+            self._writeProcessorParameter(output, self._ecalRecoNames[1], "calibration_factorsMipGev", " ".join(map(str, self._outputEcalEndcapFactors)))
+        
+        if self._runRingCalibration and self._ecalRecoNames[2] and len(self._outputEcalRingFactors):
+            self._writeProcessorParameter(output, self._ecalRecoNames[2], "calibration_factorsMipGev", " ".join(map(str, self._outputEcalRingFactors)))
+    
+    """ Get the ecal barrel energy factors
+    """
+    def ecalBarrelEnergyFactors(self):
+        if not self._ecalRecoNames[0]:
+            return []
+            
+        parameter = self._marlin.getProcessorParameter(self._ecalRecoNames[0], "calibration_factorsMipGev")
+        return [float(energy) for energy in parameter.split()]
+    
+    """ Get the ecal endcap energy factors
+    """
+    def ecalEndcapEnergyFactors(self):
+        if not self._ecalRecoNames[1]:
+            return []
 
+        parameter = self._marlin.getProcessorParameter(self._ecalRecoNames[1], "calibration_factorsMipGev")
+        return [float(energy) for energy in parameter.split()]
 
+    """ Set the barrel and endcap energy factors 
+    """
+    def setEnergyFactors(self, barrelFactors, endcapFactors):
+        if barrelFactors and self._ecalRecoNames[0]:
+            self._marlin.setProcessorParameter(self._ecalRecoNames[0], "calibration_factorsMipGev", " ".join(map(str, barrelFactors)))
 
+        if endcapFactors and self._ecalRecoNames[1]:
+            self._marlin.setProcessorParameter(self._ecalRecoNames[1], "calibration_factorsMipGev", " ".join(map(str, endcapFactors)))
+
+################################################################################
+""" ILDCaloDigiEcalEnergyStep class.
+    Implementation of ecal calibration using the (DD)ILDCaloDigi processor
+    Outputs CalibrECAL and ECALEndcapCorrectionFactor constants
+"""
+class ILDCaloDigiEcalEnergyStep(EcalEnergyStep):
+    def __init__(self):
+        EcalEnergyStep.__init__(self)
+        self._ildCaloDigiName = "MyDDCaloDigi"
+
+    """ Set the ILDCaloDigi processor name
+    """
+    def setILDCaloDigiName(self, name):
+        self._ildCaloDigiName = str(name)
+    
+    """ Write step output
+    """
+    def writeOutput(self, config) :
+        output = self._getXMLStepOutput(config, create=True)
+
+        self._writeProcessorParameter(output, self._ildCaloDigiName, "CalibrECAL", " ".join(map(str, self._outputEcalBarrelFactors)))
+        ecalEndcapFactor = self._outputEcalEndcapFactors[0] / self._outputEcalBarrelFactors[0]
+        self._writeProcessorParameter(output, self._ildCaloDigiName, "ECALEndcapCorrectionFactor", str(ecalEndcapFactor))
+    
+    """ Get the current list of ecal barrel energy factors from marlin xml file 
+    """    
+    def ecalBarrelEnergyFactors(self):    
+        parameter = self._marlin.getProcessorParameter(self._ildCaloDigiName, "CalibrECAL")
+        return [float(energy) for energy in parameter.split()]
+    
+    """ Get the current list of ecal endcap energy factors from marlin xml file 
+        Actually takes the barrel factors and multiply by the constant ECALEndcapCorrectionFactor
+    """    
+    def ecalEndcapEnergyFactors(self):
+        parameter = self._marlin.getProcessorParameter(self._ildCaloDigiName, "CalibrECAL")
+        factor = self._marlin.getProcessorParameter(self._ildCaloDigiName, "ECALEndcapCorrectionFactor")
+        return [float(energy)*float(factor) for energy in parameter.split()]
+
+    """ Set the barrel/endcap energy factors
+        Set CalibrECAL to barrel factors and compute ECALEndcapCorrectionFactor 
+        from the first entry of barrel and endcap factors
+    """
+    def setEnergyFactors(self, barrelFactors, endcapFactors):
+        self._marlin.setProcessorParameter(self._ildCaloDigiName, "CalibrECAL", " ".join(map(str, barrelFactors)))
+        # ATTN : calculate endcap factor using first energy factor entry
+        ecalEndcapFactor = endcapFactors[0] / barrelFactors[0]
+        self._marlin.setProcessorParameter(self._ildCaloDigiName, "ECALEndcapCorrectionFactor", str(ecalEndcapFactor))
 
 #
