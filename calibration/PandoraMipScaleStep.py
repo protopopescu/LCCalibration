fc245872aa01e0114a9ecd34a0ba6afe6e44fddf
Ete Remi <remi.ete@desy.de>
2017-09-21 14:00:25 +0200

    Make use of _pfoAnalysisProcessor and _marlinPandoraProcessor from parent class

--- calibration/PandoraMipScaleStep.py
+++ calibration/PandoraMipScaleStep.py
@@ -27,46 +27,27 @@ class PandoraMipScaleStep(CalibrationStep) :
         self._requireSteeringFile()
         self._requireCompactFile()
         self._requireMuonFile()
-
+        
     def description(self):
         return "Calculate the EcalToGeVMip, HcalToGeVMip and MuonToGeVMip that correspond to the mean reconstructed energy of mip calorimeter hit in the respective detectors"
 
     def readCmdLine(self, parsed) :
-        # setup ecal energy calibrator
-        self._mipScaleCalibrator = PandoraAnalysisBinary(os.path.join(parsed.pandoraAnalysis, "bin/PandoraPFACalibrate_MipResponse"))
-        self._mipScaleCalibrator.addArgument("-a", self._pfoOutputFile)
-        self._mipScaleCalibrator.addArgument("-b", '10')
-        self._mipScaleCalibrator.addArgument("-c", "./PandoraMipScale_")
-
-        # setup marlin\
+        # setup marlin
         self._marlin = Marlin(parsed.steeringFile)
         gearFile = self._marlin.convertToGear(parsed.compactFile)
         self._marlin.setGearFile(gearFile)
         self._marlin.setCompactFile(parsed.compactFile)
         self._marlin.setMaxRecordNumber(parsed.maxRecordNumber)
         self._marlin.setInputFiles(self._extractFileList(parsed.lcioMuonFile, "slcio"))
-        self._marlin.setPfoAnalysisOutput(self._pfoOutputFile)
+        self._marlin.setProcessorParameter(self._pfoAnalysisProcessor, "RootFile", self._pfoOutputFile)
 
     def init(self, config) :
-        # list of processors to run
-        processors = []
-        processors.extend(["MyAIDAProcessor"]) # not sure this is needed ...
-        processors.extend(["InitDD4hep"])
-        processors.extend(["MyEcalBarrelDigi", "MyEcalBarrelReco", "MyEcalBarrelGapFiller"])
-        processors.extend(["MyEcalEndcapDigi", "MyEcalEndcapReco", "MyEcalEndcapGapFiller"])
-        processors.extend(["MyEcalRingDigi", "MyEcalRingReco"])
-        processors.extend(["MyHcalBarrelDigi", "MyHcalBarrelReco"])
-        processors.extend(["MyHcalEndcapDigi", "MyHcalEndcapReco"])
-        processors.extend(["MyHcalRingDigi", "MyHcalRingReco"])
-        processors.extend(["MySimpleBCalDigi", "MySimpleLCalDigi", "MySimpleLHCalDigi", "MySimpleMuonDigi"])
-        processors.extend(["MyPfoAnalysis"])
-        
         self._cleanupElement(config)
         self._marlin.loadInputParameters(config)
-        self._marlin.loadStepOutputParameters(config, "MipScale")
-        self._marlin.loadStepOutputParameters(config, "EcalEnergy")
-        self._marlin.loadStepOutputParameters(config, "HcalEnergy")
-        self._marlin.turnOffProcessorsExcept(processors)
+        self._loadStepOutputs(config)
+        
+        if len(self._runProcessors):
+            self._marlin.turnOffProcessorsExcept(self._runProcessors)
         
     def run(self, config) :
         self._marlin.run()
@@ -80,10 +61,8 @@ class PandoraMipScaleStep(CalibrationStep) :
         self._outputHcalToGeVMip = mipScaleCalibrator.getHcalToGeVMip()
         self._outputMuonToGeVMip = mipScaleCalibrator.getMuonToGeVMip()
 
-
     def writeOutput(self, config) :
-
         output = self._getXMLStepOutput(config, create=True)
-        self._writeProcessorParameter(output, "MyDDMarlinPandora", "ECalToMipCalibration", self._outputEcalToGeVMip)
-        self._writeProcessorParameter(output, "MyDDMarlinPandora", "HCalToMipCalibration", self._outputHcalToGeVMip)
-        self._writeProcessorParameter(output, "MyDDMarlinPandora", "MuonToMipCalibration", self._outputMuonToGeVMip)
+        self._writeProcessorParameter(output, self._marlinPandoraProcessor, "ECalToMipCalibration", self._outputEcalToGeVMip)
+        self._writeProcessorParameter(output, self._marlinPandoraProcessor, "HCalToMipCalibration", self._outputHcalToGeVMip)
+        self._writeProcessorParameter(output, self._marlinPandoraProcessor, "MuonToMipCalibration", self._outputMuonToGeVMip)
